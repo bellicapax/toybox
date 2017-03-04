39ef8f3fbad8762a4a67de9c5a09dac058380a0e
Eris Koleszar <eris.koleszar@gmail.com>
2017-03-04 01:51:18 -0600

    Button optimizations

--- plugins/button.js
+++ plugins/button.js
@@ -1,86 +1,81 @@
 
 var buttonToyboxPlugin = {
  	name: "button",
-    toyboxType: "block",
+  toyboxType: "block",
 
  	preload: function(toyboxObject){
- 		toyboxObject._game.load.spritesheet("button", "../../assets/sprites/buttonsSheet.png", 16, 16);
-        toyboxObject._game.load.audio("buttonClick", "../../assets/sfx/door-1.wav");
+ 		 toyboxObject._game.load.spritesheet("button", "../../assets/sprites/buttonsSheet.png", 16, 16);
+     toyboxObject._game.load.audio("buttonClick", "../../assets/sfx/door-1.wav");
  	},
 
- 	sfx: ["buttonClick"],
+  sfx: ["buttonClick"],
 
  	create: function(buttonOptions){
- 		buttonOptions.spriteName = "button";
-     	
-        var validColors = ["yellow","red","blue","green"];
+      buttonOptions.spriteName = "button";
 
-        var randomizeButton = function() {
-            return validColors[Phaser.Math.between(0,(validColors.length - 1))];
-        }
+      var validColors = ["yellow","red","blue","green"];
 
-        if (typeof(buttonOptions.color) == "undefined" || validColors.indexOf(buttonOptions.color) == -1){
-            buttonOptions.color = randomizeButton();
-        }
+      var randomizeButton = function() {
+          return validColors[Phaser.Math.between(0,(validColors.length - 1))];
+      }
+
+      if (typeof(buttonOptions.color) == "undefined" || validColors.indexOf(buttonOptions.color) == -1){
+          buttonOptions.color = randomizeButton();
+      }
 
      	buttonOptions.name = buttonOptions.color + "button";
 
-        switch (buttonOptions.color){
-            case "yellow":
-                buttonOptions.spriteIndex = 0;
-            break;
-            case "red":
-                buttonOptions.spriteIndex = 2;
-            break;
-            case "blue":
-                buttonOptions.spriteIndex = 3;
-            break;
-            default:
-                buttonOptions.spriteIndex = 1; 
-            break;
-        }
+      switch (buttonOptions.color){
+          case "yellow":
+              buttonOptions.spriteIndex = 0;
+          break;
+          case "red":
+              buttonOptions.spriteIndex = 2;
+          break;
+          case "blue":
+              buttonOptions.spriteIndex = 3;
+          break;
+          default:
+              buttonOptions.spriteIndex = 1;
+          break;
+      }
 
      	var buttonCollide = function(button, collidedSprite){
-            var onTop = ( collidedSprite.y + (collidedSprite.height / 2) < button.y + (button.height / 2))
-     		if( onTop && !button.depressed){
+        var bottomOfCollided = collidedSprite.y + (collidedSprite.height / 2);
+        var topOfButton = button.y + (button.height / 2);
+        var onTop = bottomOfCollided < topOfButton;
+        var falling = collidedSprite.body.velocity.y > 0;
+     		if( onTop && falling && !button.depressed){
    				button.press();
      		}
      	}
 
-        buttonOptions.collide = buttonCollide;
+      buttonOptions.collide = buttonCollide;
 
-        var buttonUpdate = function(){
-            if (this.body == null){
-                return;
-            }
-            if (this.body.touching.down){
-                this.body.velocity.x = 0;
-            }
-        }
-
-        buttonOptions.update = buttonUpdate;
-
-        buttonOptions.onPress = (typeof(buttonOptions.onPress) == "function") ? buttonOptions.onPress : function(){};
+      buttonOptions.onPress = (typeof(buttonOptions.onPress) == "function") ? buttonOptions.onPress : function(){};
 
      	var buttonGO = this.toybox.add.block(buttonOptions);
      	buttonGO.depressed = false;
-        buttonGO.onPress = buttonOptions.onPress;
-        buttonGO.animations.add("unpressed", [buttonOptions.spriteIndex], this.toybox.animationFPS, true);
-        buttonGO.animations.add("pressed", [buttonOptions.spriteIndex + 4], this.toybox.animationFPS, true);
+      buttonGO.onPress = buttonOptions.onPress;
+      buttonGO.animations.add("unpressed", [buttonOptions.spriteIndex], this.toybox.animationFPS, true);
+      buttonGO.animations.add("pressed", [buttonOptions.spriteIndex + 4], this.toybox.animationFPS, true);
 
      	buttonGO.press = function(){
      		this.depressed = true;
      		this.toybox.sfx.buttonClick.play();
-            this.animations.play("pressed");
-            this.onPress();
-     		var thisButton = this;
-     		this.toybox.game.time.events.add(2000, function(){ 
-                thisButton.depressed = false;
+        this.animations.play("pressed");
+        this.body.checkCollision.up = false;
+        this.onPress();
+     		this.toybox.game.time.events.add(1000, function(){
+                this.depressed = false;
                 this.animations.play("unpressed");
+                this.body.checkCollision.up = true;
             }, this);
      	};
 
+      buttonGO.body.checkCollision = {none: false, any: true, up: true, down: true, left: false, right: false};
+
      	return buttonGO;
  	}
-     
+
 };
