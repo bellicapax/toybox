bd9070c5f604d482708c9e60cb01ed8604375ab5
Eris Koleszar <eris@skyvu.net>
2017-03-24 20:16:14 -0500

    Ensure fan blows wherever it is instead of only where it originated

--- plugins/fan.js
+++ plugins/fan.js
@@ -5,32 +5,36 @@ var fanToyboxPlugin = {
         toyboxObject._game.load.spritesheet("fan", "../../assets/sprites/fan.png", 32, 16);
     },
     create: function (fanOptions) {
-        fanOptions.height = fanOptions.height || 64;
+        fanOptions.height = fanOptions.height || 100;
         fanOptions.blowStrength = fanOptions.blowStrength || 50;
         fanOptions.spriteName = "fan";
 
         var blowObjects = function() {
             gos = this.toybox.currentGameObjects;
+            var blowRect = new Phaser.Rectangle(this.x - this.width * 0.5, this.y - this.blowHeight - this.height * 0.5, this.width, this.blowHeight);
             for (var i = 0; i < gos.length; i++) {
-                if (this.blowRect.contains(gos[i].x, gos[i].y)) {
+                if (blowRect.contains(gos[i].x, gos[i].y)) {
                     var normalizedDir = new Phaser.Point(0, -1);
                     var dist = Phaser.Math.distance(this.x, this.y, gos[i].x, gos[i].y);
-                    var distanceModifier = Phaser.Math.linear(1, 0.25, dist / this.blowRect.height);
+                    var distanceModifier = Phaser.Math.linear(1, 0.25, dist / blowRect.height);
                     gos[i].body.velocity.x += normalizedDir.x * this.blowStrength * distanceModifier;
                     gos[i].body.velocity.y += normalizedDir.y * this.blowStrength * distanceModifier;
                 }
             }
+            if(fanOptions.draw){
+              this.graphics.clear();
+              this.graphics.beginFill(0xffffff);
+              this.graphics.drawRect(blowRect.x, blowRect.y, blowRect.width, blowRect.height);
+            }
         }
         fanOptions.update = blowObjects;
         var fanGO = this.toybox.add.toyboxObject(fanOptions);
-        fanGO.blowRect = new Phaser.Rectangle(fanGO.x - fanGO.width * 0.5, fanGO.y - fanOptions.height - fanGO.height * 0.5, fanGO.width, fanOptions.height);
+        fanGO.blowHeight = fanOptions.height;
         fanGO.blowStrength = fanOptions.blowStrength;
         fanGO.animations.add("blow", [0,1,2,3], 24, true);
         fanGO.animations.play("blow");
         if(fanOptions.draw){
-          var g = this.toybox.game.add.graphics(0,0);
-          g.beginFill(0xffffff);
-          g.drawRect(fanGO.blowRect.x, fanGO.blowRect.y, fanGO.blowRect.width, fanGO.blowRect.height);
+          fanGO.graphics = this.toybox.game.add.graphics(0,0);
         }
         return fanGO;
     }
