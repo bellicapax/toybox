a400611e7dcc72867b27056450acd3d3f2b69fcb
Andrew Yolland <and.t.yoll@gmail.com>
2017-03-25 15:04:27 -0500

    fixed typo in Jelly
    undefined vs "undefined"

--- plugins/jelly.js
+++ plugins/jelly.js
@@ -105,7 +105,7 @@ var jellyToyboxPlugin = {
 
             var targetPoint = this.findTarget();
 
-            if( typeof(targetPoint) == undefined ){
+            if( typeof(targetPoint) == "undefined"){
                 return
             } else if( (targetPoint.x < this.x && this.xDir == 1) || (targetPoint.x > this.x && this.xDir == -1) ){
                 this.turnAround();
