8e3ee6cae8d7b60ef309cad79218d6d4e5f92784
Eris Koleszar <eris@skyvu.net>
2017-04-12 18:37:46 -0500

    Cleanup game file. Starting code to reset the game.

--- examples/leafBlower/leafBlower.js
+++ examples/leafBlower/leafBlower.js
@@ -1,32 +1,3 @@
-// Below mapping applies to XBOX 360 Wired and Wireless controller on Google Chrome (tested on Windows 7).
-// - Firefox uses different map! Separate amount of buttons and axes. DPAD = axis and not a button.
-// In other words - discrepancies when using gamepads.
-// Phaser.Gamepad.XBOX360_A = 0;
-// Phaser.Gamepad.XBOX360_B = 1;
-// Phaser.Gamepad.XBOX360_X = 2;
-// Phaser.Gamepad.XBOX360_Y = 3;
-// Phaser.Gamepad.XBOX360_LEFT_BUMPER = 4;
-// Phaser.Gamepad.XBOX360_RIGHT_BUMPER = 5;
-// Phaser.Gamepad.XBOX360_LEFT_TRIGGER = 6;
-// Phaser.Gamepad.XBOX360_RIGHT_TRIGGER = 7;
-// Phaser.Gamepad.XBOX360_BACK = 8;
-// Phaser.Gamepad.XBOX360_START = 9;
-// Phaser.Gamepad.XBOX360_STICK_LEFT_BUTTON = 10;
-// Phaser.Gamepad.XBOX360_STICK_RIGHT_BUTTON = 11;
-//
-// Phaser.Gamepad.XBOX360_DPAD_LEFT = 14;
-// Phaser.Gamepad.XBOX360_DPAD_RIGHT = 15;
-// Phaser.Gamepad.XBOX360_DPAD_UP = 12;
-// Phaser.Gamepad.XBOX360_DPAD_DOWN = 13;
-//
-// //  On FF 0 = Y, 1 = X, 2 = Y, 3 = X, 4 = left bumper, 5 = dpad left, 6 = dpad right
-// Phaser.Gamepad.XBOX360_STICK_LEFT_X = 0;
-// Phaser.Gamepad.XBOX360_STICK_LEFT_Y = 1;
-// Phaser.Gamepad.XBOX360_STICK_RIGHT_X = 2;
-// Phaser.Gamepad.XBOX360_STICK_RIGHT_Y = 3;
-
-const halfAngle = Phaser.Math.degToRad(15);
-const JOYSTICK_THRESHOLD = 0.15;
 var game = new Phaser.Game(1024, 576, Phaser.AUTO, '', {
 	preload: preload,
 	create: create,
@@ -43,190 +14,23 @@ var triangle = {
 	b: new Phaser.Point(),
 	c: new Phaser.Point()
 };
-var blowables = [];
 var blowStrength = 5;
+var blowables = [];
 var players = [];
-
-var containerGraphics;
 var containers = [];
+var containerGraphics;
+var playersGraphics = [];
+
 var levelSettings = {
+	numPlayers : 1,
 	numContainers: 2,
 	containerSize: 200,
 	nextblowableDelay: 3000,
 	blowableSpeedMin: 10,
 	blowableSpeedMax: 50,
-	goalsToPop : 4
+	goalsToPop : 2
 };
-var pad1;
-var pad2;
-var leftStickX = Phaser.Gamepad.XBOX360_STICK_LEFT_X;
-var leftStickY = Phaser.Gamepad.XBOX360_STICK_LEFT_Y;
-var rightStickX = Phaser.Gamepad.XBOX360_STICK_RIGHT_X;
-var rightStickY = Phaser.Gamepad.XBOX360_STICK_RIGHT_Y;
-var accelAxis = Phaser.Gamepad.XBOX360_RIGHT_TRIGGER;
-var decelAxis = Phaser.Gamepad.XBOX360_LEFT_TRIGGER;
-var blowButton = Phaser.Gamepad.XBOX360_RIGHT_BUMPER;
-var midAngle = 0;
 
-function alienAstronautUpdate() {
-	if (this.isHit) {
-		return;
-	}
-
-	if (this.health <= 0) {
-		this.kill();
-		return;
-	}
-
-	var usingGamepad = this.toybox.game.input.gamepad.padsConnected > 0;
-	var padInput = getPadInput(this.controls.pad);
-	this.body.angularVelocity = 0;
-	if (this.controls.right.isDown || padInput.leftX > JOYSTICK_THRESHOLD) {
-		var modifier = this.controls.right.isDown ? 1 : padInput.leftX;
-		this.body.angularVelocity = this.speed * modifier;
-		if (this.animations.name !== "run") {
-			this.animations.play("run");
-		}
-	} else if (this.controls.left.isDown || padInput.leftX < -JOYSTICK_THRESHOLD) {
-		var modifier = this.controls.left.isDown ? 1 : Math.abs(padInput.leftX);
-		this.body.angularVelocity = -this.speed * modifier;
-		if (this.animations.name !== "run") {
-			this.animations.play("run");
-		}
-	}
-
-	// checkForBoost
-	var angularspeed = new Phaser.Point();
-	if (this.controls.up.isDown || padInput.accel > JOYSTICK_THRESHOLD) {
-		this.toybox.game.physics.arcade.velocityFromAngle(this.angle + 90, this.jumpForce, angularspeed);
-		var modifier = this.controls.up.isDown ? 1 : padInput.accel;
-		this.body.velocity.x -= angularspeed.x * modifier;
-		this.body.velocity.y -= angularspeed.y * modifier;
-		if (this.animations.name !== "jump") {
-			this.animations.play("jump");
-		}
-	} else if (this.controls.down.isDown || padInput.decel > JOYSTICK_THRESHOLD) {
-		this.toybox.game.physics.arcade.velocityFromAngle(this.angle + 90, this.jumpForce, angularspeed);
-		var modifier = this.controls.down.isDown ? 1 : padInput.decel;
-		this.body.velocity.x += angularspeed.x * modifier;
-		this.body.velocity.y += angularspeed.y * modifier;
-		if (this.animations.name !== "jump") {
-			this.animations.play("jump");
-		}
-	}
-
-	var pointer = this.toybox.game.input.activePointer;
-	setMidAngle(this, pointer);
-	calculateTriangle(this);
-	var shouldBlow = pointer.isDown || padInput.blow;
-
-	drawTriangle(this.graphics, shouldBlow);
-	if (shouldBlow) {
-		var blowablesToBlow = getblowablesToBlow(this);
-		if (usingGamepad) {
-			if (Math.abs(padInput.rightX) > JOYSTICK_THRESHOLD || Math.abs(padInput.rightY)) {
-				this.normalizedDir = new Phaser.Point(padInput.rightX, padInput.rightY).normalize()
-			}
-		} else {
-			this.normalizedDir = new Phaser.Point(pointer.x - this.x, pointer.y - this.y).normalize();
-		}
-		blowblowables(blowablesToBlow, this, this.normalizedDir);
-	}
-}
-
-function getPadInput(pad) {
-	return {
-		leftX: pad.axis(leftStickX),
-		leftY: pad.axis(leftStickY),
-		rightX: pad.axis(rightStickX),
-		rightY: pad.axis(rightStickY),
-		accel: pad.buttonValue(accelAxis),
-		decel: pad.buttonValue(decelAxis),
-		blow: pad.isDown(blowButton)
-	};
-}
-
-function setMidAngle(sprite, pointer) {
-	if (sprite.toybox.game.input.gamepad.padsConnected > 0) {
-		var xAxis = sprite.controls.pad.axis(rightStickX);
-		var yAxis = sprite.controls.pad.axis(rightStickY);
-		if (Math.abs(xAxis) > JOYSTICK_THRESHOLD || Math.abs(yAxis) > JOYSTICK_THRESHOLD) {
-			midAngle = Math.atan2(yAxis, xAxis);
-		}
-	} else {
-		midAngle = Phaser.Math.angleBetween(sprite.x, sprite.y, pointer.x, pointer.y);
-	}
-}
-
-function calculateTriangle(sprite) {
-	var angleToLeft = midAngle + halfAngle;
-	var angleToRight = midAngle - halfAngle;
-	var leftDirection = new Phaser.Point(Math.cos(angleToLeft), Math.sin(angleToLeft));
-	var rightDirection = new Phaser.Point(Math.cos(angleToRight), Math.sin(angleToRight));
-	var leftPoint = new Phaser.Point(sprite.x + leftDirection.x * hypotenuse, sprite.y + leftDirection.y * hypotenuse);
-	var rightPoint = new Phaser.Point(sprite.x + rightDirection.x * hypotenuse, sprite.y + rightDirection.y * hypotenuse);
-	triangle.a = new Phaser.Point(sprite.x, sprite.y);
-	triangle.b = leftPoint;
-	triangle.c = rightPoint;
-}
-
-function drawTriangle(graphics, shouldFill) {
-	graphics.clear();
-	graphics.lineStyle(1, 0xffffff, 1);
-	if (shouldFill) {
-		graphics.fillAlpha = 0.5;
-		graphics.beginFill(0xffffff);
-	}
-	graphics.moveTo(triangle.a.x, triangle.a.y);
-	graphics.lineTo(triangle.b.x, triangle.b.y);
-	graphics.lineTo(triangle.c.x, triangle.c.y);
-	graphics.lineTo(triangle.a.x, triangle.a.y);
-}
-
-function getblowablesToBlow() {
-	var blowablesToBlow = [];
-	for (var i = 0; i < blowables.length; i++) {
-		if (pointIsInTriangle(blowables[i].x, blowables[i].y, triangle)) {
-			blowablesToBlow.push(blowables[i]);
-		}
-	}
-	return blowablesToBlow;
-}
-
-function blowblowables(blowablesToBlow, sprite, normalizedDir) {
-	for (var i = 0; i < blowablesToBlow.length; i++) {
-		var dist = Phaser.Math.distance(sprite.x, sprite.y, blowablesToBlow[i].x, blowablesToBlow[i].y);
-		var distanceModifier = Phaser.Math.linear(1, 0.1, dist / hypotenuse);
-		blowablesToBlow[i].body.velocity.x += normalizedDir.x * blowStrength * distanceModifier;
-		blowablesToBlow[i].body.velocity.y += normalizedDir.y * blowStrength * distanceModifier;
-	}
-}
-
-function pointIsInTriangle(px, py, triangle) {
-	//credit: http://www.blackpawn.com/texts/pointinpoly/default.html
-	var ax = triangle.a.x;
-	var ay = triangle.a.y;
-	var bx = triangle.b.x;
-	var by = triangle.b.y;
-	var cx = triangle.c.x;
-	var cy = triangle.c.y;
-	var v0 = [cx - ax, cy - ay];
-	var v1 = [bx - ax, by - ay];
-	var v2 = [px - ax, py - ay];
-
-	var dot00 = (v0[0] * v0[0]) + (v0[1] * v0[1]);
-	var dot01 = (v0[0] * v1[0]) + (v0[1] * v1[1]);
-	var dot02 = (v0[0] * v2[0]) + (v0[1] * v2[1]);
-	var dot11 = (v1[0] * v1[0]) + (v1[1] * v1[1]);
-	var dot12 = (v1[0] * v2[0]) + (v1[1] * v2[1]);
-
-	var invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-
-	var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-	var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-
-	return ((u >= 0) && (v >= 0) && (u + v < 1));
-}
 
 function preload() {
 	toybox = new Toybox(game, settings);
@@ -237,49 +41,28 @@ function preload() {
 
 function create() {
 	toybox.create();
-	createContainers();
 	containerGraphics = game.add.graphics(0, 0);
+	for (var i = 0; i < 4; i++) {
+		playersGraphics.push(game.add.graphics(0, 0));
+	}
+	createLevel();
+}
+
+function createLevel() {
+	createContainers();
 	drawContainers(containerGraphics);
-	createAstronaut(0);
-	// createAstronaut(1);
+	createAstronauts();
 	createBlowable();
 }
 
-function createBlowable() {
-	var speed = Phaser.Math.between(levelSettings.blowableSpeedMin, levelSettings.blowableSpeedMax);
-	var xIsZero = toybox.diceRoll(2) == 1;
-	var crateOptions = {
-		scale: 1,
-		startingX: game.world.centerX,
-		startingY: game.world.centerY,
-		allowGravity: false
-		// dX: xIsZero ? speed : 0,
-		// dY: !xIsZero ? speed : 0,
-		// update: destroyOutsideBounds
-	};
-	crate = toybox.add.crate(crateOptions);
-	crate.events.onKilled.addOnce(createBlowable);
-	crate.update = function () {};
-	crate.body.collideWorldBounds = false;
-	crate.body.drag = new Phaser.Point(0);
-	blowables.push(crate);
-	// game.time.events.add(levelSettings.nextblowableDelay, createBlowable);
+function createAstronauts() {
+	for (var i = 0; i < levelSettings.numPlayers; i++) {
+		createAstronaut(i);
+	}
 }
-
-// function destroyOutsideBounds() {
-// 	if (this.x < 0 || this.x > this.toybox.game.world.width || this.y < 0 || this.y > this.toybox.game.world.height) {
-// 		var index = blowables.indexOf(this);
-// 		if (index > -1) {
-// 			blowables.splice(index, 1);
-// 		}
-// 		this.kill();
-// 	}
-// }
 
 function createAstronaut(playerNo) {
 	var alienAstronautOptions = {
-		// allowGravity: false,
-		// update: alienAstronautUpdate,
 		startingX: containers[playerNo].x,
 		startingY: containers[playerNo].y,
 		flyForce: 2.25,
@@ -288,9 +71,7 @@ function createAstronaut(playerNo) {
 		pad : game.input.gamepad["pad" + (playerNo + 1).toString()]
 	};
 	var player = toybox.add.alienAstronaut(alienAstronautOptions);
-	player.controls.pad = game.input.gamepad.pad1;
-	player.body.allowGravity = false;
-	player.graphics = game.add.graphics(0, 0);
+	player.graphics = playersGraphics[playerNo];
 	players.push(player);
 }
 
@@ -311,6 +92,23 @@ function getContainerPositionForPlayer(player) {
 	point.x = player == 0 ? levelSettings.containerSize * 0.5 : game.world.width - levelSettings.containerSize;
 	point.y = game.world.centerY;
 	return point;
+}
+
+function createBlowable() {
+	var speed = Phaser.Math.between(levelSettings.blowableSpeedMin, levelSettings.blowableSpeedMax);
+	var xIsZero = toybox.diceRoll(2) == 1;
+	var crateOptions = {
+		scale: 1,
+		startingX: game.world.centerX,
+		startingY: game.world.centerY,
+		allowGravity: false
+	};
+	crate = toybox.add.crate(crateOptions);
+	crate.events.onKilled.addOnce(createBlowable);
+	crate.update = function () {};
+	crate.body.collideWorldBounds = false;
+	crate.body.drag = new Phaser.Point(0);
+	blowables.push(crate);
 }
 
 function drawContainers(graphics) {
@@ -371,5 +169,14 @@ function updateContainerSize(container) {
 	}
 	else {
 		console.log("You won!");
+		clearLevel();
+		createLevel();
 	}
+}
+
+function clearLevel() {
+	players = [];
+	containers = [];
+	blowables = [];
+	toybox.clear();
 }
--- plugins/alienAstronaut.js
+++ plugins/alienAstronaut.js
@@ -19,7 +19,7 @@ var alienAstronautToyboxPlugin = {
     name: "alienAstronaut",
     toyboxType: "player",
 
-    preload: function(toyboxObject) {
+    preload: function (toyboxObject) {
         toyboxObject._game.load.spritesheet("greenAlien", "../../assets/sprites/greenAlienSheet.png", 16, 20);
         toyboxObject._game.load.spritesheet("blueAlien", "../../assets/sprites/blueAlienSheet.png", 16, 20);
         toyboxObject._game.load.spritesheet("pinkAlien", "../../assets/sprites/pinkAlienSheet.png", 16, 20);
@@ -32,8 +32,8 @@ var alienAstronautToyboxPlugin = {
 
     sfx: ["alienJump", "alienHit", "alienKill"],
 
-    create: function(alienOptions) {
-        alienOptions = typeof(alienOptions) == "undefined" ? {} : alienOptions;
+    create: function (alienOptions) {
+        alienOptions = typeof (alienOptions) == "undefined" ? {} : alienOptions;
         const halfAngle = Phaser.Math.degToRad(15);
         const JOYSTICK_THRESHOLD = 0.15;
         var leftStickX = Phaser.Gamepad.XBOX360_STICK_LEFT_X;
@@ -54,10 +54,10 @@ var alienAstronautToyboxPlugin = {
         alienOptions.health = alienOptions.health || 3;
         var validColors = ["green", "blue", "pink"];
         alienOptions.health = alienOptions.health || 3;
-        if (typeof(alienOptions.color) == "undefined" || validColors.indexOf(alienOptions.color) == -1) {
+        if (typeof (alienOptions.color) == "undefined" || validColors.indexOf(alienOptions.color) == -1) {
             alienOptions.color = "green";
         }
-        if (typeof(alienOptions.controls) == "undefined") {
+        if (typeof (alienOptions.controls) == "undefined") {
             alienOptions.controls = {
                 left: 65,
                 right: 68,
@@ -67,7 +67,7 @@ var alienAstronautToyboxPlugin = {
         };
         alienOptions.spriteName = alienOptions.color + "Alien";
 
-        var updateAlienAstronaut = function() {
+        var updateAlienAstronaut = function () {
             if (this.isHit) {
                 return;
             }
@@ -81,7 +81,7 @@ var alienAstronautToyboxPlugin = {
             updatePlayerFan(this, padInput);
         };
 
-        var getPadInput = function(pad) {
+        var getPadInput = function (pad) {
             return {
                 leftX: pad.axis(leftStickX),
                 leftY: pad.axis(leftStickY),
@@ -95,7 +95,7 @@ var alienAstronautToyboxPlugin = {
 
 
 
-        var updateAlienMovement = function(player, padInput) {
+        var updateAlienMovement = function (player, padInput) {
             var maxSecondsToBrake = player.maxSecondsToBrake || 1.5;
             var maxSpeed = maxSecondsToBrake * 60 * Math.abs(player.flyForce);
             player.body.angularVelocity = 0;
@@ -105,8 +105,7 @@ var alienAstronautToyboxPlugin = {
                 if (player.animations.name !== "run") {
                     player.animations.play("run");
                 }
-            }
-            else if (player.controls.left.isDown || padInput.leftX < -JOYSTICK_THRESHOLD) {
+            } else if (player.controls.left.isDown || padInput.leftX < -JOYSTICK_THRESHOLD) {
                 var modifier = player.controls.left.isDown ? 1 : Math.abs(padInput.leftX);
                 player.body.angularVelocity = -player.speed * modifier;
                 if (player.animations.name !== "run") {
@@ -126,8 +125,7 @@ var alienAstronautToyboxPlugin = {
                 if (player.animations.name !== "jump") {
                     player.animations.play("jump");
                 }
-            }
-            else if (player.controls.down.isDown || padInput.decel > JOYSTICK_THRESHOLD) {
+            } else if (player.controls.down.isDown || padInput.decel > JOYSTICK_THRESHOLD) {
                 player.toybox.game.physics.arcade.velocityFromAngle(player.angle + 90, player.flyForce, angularspeed);
                 var modifier = player.controls.down.isDown ? 1 : padInput.decel;
                 var unclampedVeloX = player.body.velocity.x + angularspeed.x * modifier;
@@ -140,7 +138,7 @@ var alienAstronautToyboxPlugin = {
             }
         };
 
-        var updatePlayerFan = function(player, padInput) {
+        var updatePlayerFan = function (player, padInput) {
             var usingGamepad = player.toybox.game.input.gamepad.padsConnected > 0;
             var pointer = player.toybox.game.input.activePointer;
             var midAngle = getMidAngle(player, pointer, usingGamepad);
@@ -150,7 +148,7 @@ var alienAstronautToyboxPlugin = {
             tryBlowSprites(player, padInput, pointer, usingGamepad, triangle, shouldBlow);
         };
 
-        var getMidAngle = function(player, pointer, usingGamepad) {
+        var getMidAngle = function (player, pointer, usingGamepad) {
             var midAngle;
             if (usingGamepad) {
                 var xAxis = player.controls.pad.axis(rightStickX);
@@ -158,14 +156,13 @@ var alienAstronautToyboxPlugin = {
                 if (Math.abs(xAxis) > JOYSTICK_THRESHOLD || Math.abs(yAxis) > JOYSTICK_THRESHOLD) {
                     midAngle = Math.atan2(yAxis, xAxis);
                 }
-            }
-            else {
+            } else {
                 midAngle = Phaser.Math.angleBetween(player.x, player.y, pointer.x, pointer.y);
             }
             return midAngle;
         }
 
-        var calculateTriangle = function(midAngle, sprite) {
+        var calculateTriangle = function (midAngle, sprite) {
             var angleToLeft = midAngle + halfAngle;
             var angleToRight = midAngle - halfAngle;
             var leftDirection = new Phaser.Point(Math.cos(angleToLeft), Math.sin(angleToLeft));
@@ -179,7 +176,7 @@ var alienAstronautToyboxPlugin = {
             };
         };
 
-        var drawTriangle = function(triangle, graphics, shouldFill) {
+        var drawTriangle = function (triangle, graphics, shouldFill) {
             graphics.clear();
             graphics.lineStyle(1, 0xffffff, 1);
             if (shouldFill) {
@@ -192,22 +189,21 @@ var alienAstronautToyboxPlugin = {
             graphics.lineTo(triangle.a.x, triangle.a.y);
         };
 
-        var tryBlowSprites = function(player, padInput, pointer, usingGamepad, triangle, shouldBlow) {
+        var tryBlowSprites = function (player, padInput, pointer, usingGamepad, triangle, shouldBlow) {
             if (shouldBlow) {
                 var spritesToBlow = getSpritesToBlow(triangle, player);
                 if (usingGamepad) {
                     if (Math.abs(padInput.rightX) > JOYSTICK_THRESHOLD || Math.abs(padInput.rightY)) {
                         player.normalizedDir = new Phaser.Point(padInput.rightX, padInput.rightY).normalize()
                     }
-                }
-                else {
+                } else {
                     player.normalizedDir = new Phaser.Point(pointer.x - player.x, pointer.y - player.y).normalize();
                 }
                 blowSprites(spritesToBlow, player, player.normalizedDir);
             }
         }
 
-        var getSpritesToBlow = function(triangle, player) {
+        var getSpritesToBlow = function (triangle, player) {
             var spritesToBlow = [];
             for (var i = 0; i < blowables.length; i++) {
                 if (pointIsInTriangle(blowables[i].x, blowables[i].y, triangle)) {
@@ -217,26 +213,52 @@ var alienAstronautToyboxPlugin = {
             return spritesToBlow;
         }
 
-        var blowSprites = function(spritesToBlow, sprite, normalizedDir) {
+        var pointIsInTriangle = function pointIsInTriangle(px, py, triangle) {
+            //credit: http://www.blackpawn.com/texts/pointinpoly/default.html
+            var ax = triangle.a.x;
+            var ay = triangle.a.y;
+            var bx = triangle.b.x;
+            var by = triangle.b.y;
+            var cx = triangle.c.x;
+            var cy = triangle.c.y;
+            var v0 = [cx - ax, cy - ay];
+            var v1 = [bx - ax, by - ay];
+            var v2 = [px - ax, py - ay];
+
+            var dot00 = (v0[0] * v0[0]) + (v0[1] * v0[1]);
+            var dot01 = (v0[0] * v1[0]) + (v0[1] * v1[1]);
+            var dot02 = (v0[0] * v2[0]) + (v0[1] * v2[1]);
+            var dot11 = (v1[0] * v1[0]) + (v1[1] * v1[1]);
+            var dot12 = (v1[0] * v2[0]) + (v1[1] * v2[1]);
+
+            var invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+
+            var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+            var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+            return ((u >= 0) && (v >= 0) && (u + v < 1));
+
+        };
+
+        var blowSprites = function (spritesToBlow, sprite, normalizedDir) {
             for (var i = 0; i < spritesToBlow.length; i++) {
                 var dist = Phaser.Math.distance(sprite.x, sprite.y, spritesToBlow[i].x, spritesToBlow[i].y);
                 var distanceModifier = Phaser.Math.linear(1, 0.1, dist / hypotenuse);
                 spritesToBlow[i].body.velocity.x += normalizedDir.x * blowStrength * distanceModifier;
                 spritesToBlow[i].body.velocity.y += normalizedDir.y * blowStrength * distanceModifier;
             }
-        }
+        };
 
-        alienOptions.update = typeof(alienOptions.update) != "function" ? updateAlienAstronaut : alienOptions.update;
+        alienOptions.update = typeof (alienOptions.update) != "function" ? updateAlienAstronaut : alienOptions.update;
 
-        var alienCollide = function(alien, collidedSprite) {
+        var alienCollide = function (alien, collidedSprite) {
             var alienIsOnTop = (alien.y + (alien.height / 2)) <= (collidedSprite.y - collidedSprite.height / 2);
 
             if (collidedSprite.isMob()) {
                 if (alienIsOnTop) {
                     alien.body.velocity.y = -200;
                     collidedSprite.hit();
-                }
-                else {
+                } else {
                     if (collidedSprite.health > 0) {
                         alien.hit();
                     }
@@ -246,7 +268,7 @@ var alienAstronautToyboxPlugin = {
 
         alienOptions.collide = alienCollide;
 
-        var alienKill = function(alien) {
+        var alienKill = function (alien) {
             var splosion = this.toybox.game.add.emitter(alien.x, alien.y, 12);
             this.toybox.topDecorations.add(splosion);
             splosion.makeParticles('heartsAndStar', [5]);
@@ -256,7 +278,7 @@ var alienAstronautToyboxPlugin = {
             this.toybox.sfx.alienKill.play();
             splosion.start(true, 4000, null, 12);
             this.toybox.players.remove(this);
-            game.time.events.add(2000, function() {
+            game.time.events.add(2000, function () {
                 splosion.kill()
             }, this);
 
@@ -270,11 +292,11 @@ var alienAstronautToyboxPlugin = {
         alienGO.health = alienOptions.health;
         alienGO.events.onHit = new Phaser.Signal();
         alienGO.controls.pad = alienOptions.pad || this.toybox.game.input.gamepad.pad1;
-        if (typeof(alienOptions.onHit) == "function") {
+        if (typeof (alienOptions.onHit) == "function") {
             alienGO.events.onHit.add(alienOptions.onHit);
         }
 
-        alienGO.hit = function() {
+        alienGO.hit = function () {
             if (this.isHit) {
                 return;
             }
@@ -289,11 +311,10 @@ var alienAstronautToyboxPlugin = {
             this.toybox.sfx.alienHit.play();
             this.events.onHit.dispatch(this);
             var thisAlien = this;
-            this.toybox.game.time.events.add(500, function() {
+            this.toybox.game.time.events.add(500, function () {
                 if (thisAlien.health <= 0) {
                     thisAlien.kill();
-                }
-                else {
+                } else {
                     thisAlien.animations.play("idle");
                     thisAlien.isHit = false;
                 }
